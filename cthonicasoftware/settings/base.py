d720f9b86bb6342644c5c3d6cdeae4fe481c9887
Chris <christopher.metcalf@proton.me>
2026-03-01 11:32:58 -0500

    Fixed static file location error

--- cthonicasoftware/settings/base.py
+++ cthonicasoftware/settings/base.py
@@ -68,7 +68,7 @@ TEMPLATES = [
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "../../static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "../static")]
 
 WSGI_APPLICATION = 'cthonicasoftware.wsgi.application'
 
